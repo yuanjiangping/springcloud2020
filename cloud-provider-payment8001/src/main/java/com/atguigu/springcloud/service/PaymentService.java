b0cdc92522a2459a43523b2196cb3e1725e93bec
samo_ <samo_yuan@126.com>
2020-07-17 21:46:51 +0800

    测试

--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -5,7 +5,15 @@ import org.apache.ibatis.annotations.Param;
 
 public interface PaymentService {
 
+    /**
+     * @param payment
+     * @return
+     */
     public int create(Payment payment);
 
+    /**
+     * @param id  根据id查询订单信息
+     * @return
+     */
     public Payment getPaymentById(@Param("id") Long id);
 }
